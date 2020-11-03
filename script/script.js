1e99e5b0a5f586ba0f0936adaced63ffd79b0819
Федор Краснов <hitomizavr@gmail.com>
2020-11-04 00:55:57 +0300

    style: fix bem class

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 let popup = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__container');
 let btnClose = document.querySelector('.popup__btn-close');
-let popupName = document.querySelector('.popup-form__input_name');
-let popupAbout = document.querySelector('.popup-form__input_description');
+let popupName = document.querySelector('.popup-form__input_field_name');
+let popupAbout = document.querySelector('.popup-form__input_field_description');
 
 let btnEdit = document.querySelector('.profile-info__btn-edit');
 let profileName = document.querySelector('.profile-info__name');
